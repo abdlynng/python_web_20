2bee4a55fc836215f9f0ba73446e437a34e0d434
Abdoulaye Niang <abdlynng@gmail.com>
2020-09-03 22:27:29 +0000

    correction application covoiturage fichier 0001_initial.py et models.py

--- covoiturage/models.py
+++ covoiturage/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Personne(models.Model):
-    prenom = models.CharField(max_length=30)
-    nom = models.CharField(max_length=30)
     
     class Meta:
-        abstract = True
+	    abstract=True
 
+    prenom = models.CharField(max_length=30)
+    nom = models.CharField(max_length=30)
 
 class Conducteur(Personne):
     nbre_place = models.IntegerField()
@@ -16,11 +16,9 @@ class Conducteur(Personne):
 class Passager(Personne):
     pass
 
-
 class Trajet(models.Model):
     depart = models.CharField(max_length=10)
     arrive = models.CharField(max_length=10)
     prix = models.PositiveIntegerField(default=4000)
-    
     passagers = models.ManyToManyField(Passager)
     conducteur = models.ForeignKey(Conducteur,on_delete=models.CASCADE)
