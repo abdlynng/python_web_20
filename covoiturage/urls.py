242e1dd10c17dc26f3c69bf77ceb6a2f6458fe96
Abdoulaye Niang <abdlynng@gmail.com>
2020-09-04 20:45:15 +0000

    les views conducteur, trajet ajoutes dans views.py et leur urls dans urls.py

--- covoiturage/urls.py
+++ covoiturage/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views
+from . import views 
 
 urlpatterns = [
+    path('trajet/<int:pk>/', views.trajet, name='trajet'),
+    path('conducteur/<int:pk>/', views.conducteur, name='conducteur'),
     path('', views.index, name='index'),
 ]
 
--- covoiturage/views.py
+++ covoiturage/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Trajet
+from .models import  Conducteur
+from .models import  Passager
 
+from django.template import loader
 # Create your views here.
 def index(request):
-    return HttpResponse("COVOITURAGE URBAINE")
+    return HttpResponse("COVOITURAGE URBAINE Welcome")
+
+def trajet(request, pk):
+    t = Trajet.objects.get(id=pk)
+    c = Conducteur.objects.get(id=t.id)
+    p = []
+    p.append(t.depart)
+    p.append(t.arrive)
+    p.append(str(t.prix))
+    p.append(c.prenom)
+    p.append(c.nom)
+    m = ' | '.join(p)
+    #p = t.depart +" | "+ t.arrive +" | "+str(t.prix) +" | "+ c.prenom+" | "+ c.nom +" |"
+    return HttpResponse(m)
+
+
+def conducteur(request,pk):
+    c = Conducteur.objects.get(id=pk)
+    p = c.prenom +" | "+ c.nom +" | "+str(c.nbre_place) +" | "
+    return HttpResponse(p)
